73fd58ed595beec84618f139511a50171488dc78
Ali Shah <s.ashah@outlook.com>
2020-10-28 21:05:46 -0400

    [Update] Finished tutorial step 4

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ var dy = -2;
 
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -16,18 +23,61 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight); // Can use a paddleY variable
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBall();
+  drawPaddle();
+
   x += dx;
   y += dy;
 
   if (y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
     dy = -dy;
   }
-  
+
   if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
     dx = -dx;
+  }
+
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth;
+    }
+  }
+  else if (leftPressed) { // Can change this to just "if" to balance left and right forces
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
   }
 }
 
